4588418c87150c47a97f9521f47368079109641e
Guilherme Almeida <89315545+VegaCenturion@users.noreply.github.com>
2022-07-10 18:34:06 -0300

    Add files via upload

---
+++ Estrela.java
@@ -0,0 +1,32 @@
+
+public class Estrela {
+
+    private String nome;
+
+    public int p1;
+
+    public static int p2;
+
+    public String getNome() {
+
+        return nome;
+
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+
+    }
+
+    public Estrela() {
+
+        p1++;
+
+        p2++;
+
+    }
+
+}
+
+
