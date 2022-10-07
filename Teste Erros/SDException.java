923d35523b6f2adeb2f45a6362cfaa9550a785b8
Guilherme Almeida <89315545+VegaCenturion@users.noreply.github.com>
2022-10-07 14:50:36 -0600

    Add files via upload

---
+++ Teste Erros/SDException.java
@@ -0,0 +1,8 @@
+package Pacote;
+
+public class SDException extends RuntimeException{
+	
+	public SDException(String msg){
+		super(msg);
+	}
+}
---
+++ Teste Erros/Utils.java
@@ -0,0 +1,8 @@
+package Pacote;
+
+public class Utils { 
+	public static double liberarDinheiro(double valor){	
+		System.out.println("Foi liberado a quantia de R$" + Double.toString(valor) + "\n");
+		return valor;
+	}
+}
